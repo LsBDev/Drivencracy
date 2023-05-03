127d04002757c09573ee3968abb5cd4554cd8be3
LsB.Dev <prof.luifel@gmail.com>
2023-05-03 00:44:52 -0300

    configuração inicial do projeto

---
+++ routes/poll.routes.js
@@ -0,0 +1,10 @@
+import { Router } from "express"
+// import validadeSchema
+// importar esquemas e controllers
+
+const pollRoutes = Router()
+
+// pollRouter.post("/poll", controller)
+// pollRouter.get("/poll", controller)
+// pollRouter.get("/poll/:id/choice", controller)
+// pollRouter.get("/poll/:id/result", controller)
---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express, { Router, json } from "express"
+import cors from "cors"
+import router from "../routes/index.routes"
+
+const app = express()
+app.use(json())
+app.use(cors())
+app.use(router)
+
+
+const PORT = 5000
+app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`))
